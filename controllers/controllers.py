62368df60039c4bf45c6d0439225b1fbfd750761
dunmi9791 <dominic.anyanna@gmail.com>
2022-07-11 17:58:12 +0100

    facility staffs

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,5 +1,84 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
+from datetime import datetime
+from datetime import date , time, timedelta
+from dateutil import relativedelta
+from dateutil.relativedelta import relativedelta
+
+
+class MobileApiSamples(http.Controller):
+    @http.route('/samples/', website=True, type='json', auth='user')
+    def samplesall(self):
+        # return "List of all Samples"
+        samples_list = []
+        samples_all = request.env['sample.transport'].search([])
+        for rec in samples_all:
+            vals = {
+                'id': rec.id,
+                'name': rec.st_no,
+                'specimen_type': rec.specimen_type,
+                'total_samples': rec.total_samples_sent,
+                'temperature_at_send': rec.temperature_send,
+
+            }
+            samples_list.append(vals)
+
+        data = {'status': 200, 'response': samples_list, 'message': 'Samples Returned'}
+        return data
+
+class MobileApiTestTypes(http.Controller):
+    @http.route('/testtypes/', website=True, type='json', auth='user')
+    def testtypes(self):
+        # return "List of all Samples"
+        tests_list = []
+        tests_all = request.env['test.type'].search([])
+        for rec in tests_all:
+            vals = {
+                'id': rec.id,
+                'name': rec.name,
+
+            }
+            tests_list.append(vals)
+
+        data = {'status': 200, 'response': tests_list, 'message': 'Test Types Returned'}
+        return data
+
+
+class MobileApiFacility(http.Controller):
+    @http.route('/facilities/', website=True, type='json', auth='user')
+    def facilitiesall(self):
+        # return "List of all Samples"
+        facilities_list = []
+        facilities_all = request.env['lab.facility'].search([])
+        for rec in facilities_all:
+            vals = {
+                'id': rec.id,
+                'name': rec.name,
+
+            }
+            facilities_list.append(vals)
+
+        data = {'status': 200, 'response': facilities_list, 'message': 'Facilities Returned'}
+        return data
+
+
+class MobileFacilityStaff(http.Controller):
+    @http.route('/facilitystaffs/', website=True, type='json', auth='user')
+    def facilitystaffall(self):
+        # return "List of all Samples"
+        facilitystaffs_list = []
+        facilitystaffs_all = request.env['staff.facility'].search([])
+        for rec in facilitystaffs_all:
+            vals = {
+                'id': rec.id,
+                'name': rec.name,
+
+            }
+            facilitystaffs_list.append(vals)
+
+        data = {'status': 200, 'response': facilitystaffs_list, 'message': 'Facility Staffs Returned'}
+        return data
 
 # class RidersMobileApi(http.Controller):
 #     @http.route('/riders_mobile_api/riders_mobile_api/', auth='public')
